60e09599f68e1dc7a4516c1ca804305569e6d12c
masakiAndMarron <84778070+masakiAndMarron@users.noreply.github.com>
2024-12-28 21:43:26 +0900

    設定ファイル追加

---
+++ src/utils/CommonConstants.js
@@ -0,0 +1,13 @@
+/** 
+################################
+#        定数ファイル           #
+################################ 
+*/
+
+export const ENDPOINT_VALUE = "http://localhost:8080/";
+
+export const MAX_FILE_SIZE = 200;
+export const FILE_SIZE_OVER_MSG =
+  "ファイルサイズが大きすぎます。50MB以下のファイルをアップロードしてください。";
+export const FILE_TYPE_NOT_ALLOWED_MSG =
+  "ファイル形式が許可されていません。アップロード可能なファイル形式は.mp4 .mpeg .webm .quicktime .aviです。";
